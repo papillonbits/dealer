8d2337ef9740e09a93caf933bca53723c84c6000
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-09-29 18:54:56 +0200

    update build folder and scripts

--- packages/core/compile/constant.js
+++ packages/core/compile/constant.js
@@ -3,7 +3,7 @@ const path = require('path')
 export const compileFolderPath = path.resolve(__dirname, '../compile')
 export const nodeModulesFolderPath = path.resolve(__dirname, '../../../node_modules')
 export const srcFolderPath = path.resolve(__dirname, '../src')
-export const buildFolderPath = path.resolve(__dirname, '../dist')
+export const buildFolderPath = path.resolve(__dirname, '../build')
 export const publicFolderPath = path.resolve(__dirname, '../public')
 
 export const includedSourcePaths = [
