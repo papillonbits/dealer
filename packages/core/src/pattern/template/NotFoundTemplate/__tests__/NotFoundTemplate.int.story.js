5056eb9b8db13372f5afa8f68935418932cd9da0
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-09-30 21:19:55 +0200

    apply MemoryRouter to NotFoundTemplate story

--- packages/core/src/pattern/template/NotFoundTemplate/__tests__/NotFoundTemplate.int.story.js
+++ packages/core/src/pattern/template/NotFoundTemplate/__tests__/NotFoundTemplate.int.story.js
@@ -1,29 +1,20 @@
-import { BrowserRouter } from 'react-router-dom'
+import { MemoryRouter } from 'react-router-dom'
 import { withTests } from '@storybook/addon-jest'
 import { NotFoundTemplate } from '../NotFoundTemplate'
 import results from '../../../../../../../.jest-test-results.json'
 
-const props = {
-  location: {
-    pathname: 'fantastic-path-name',
-  },
-}
-
 export default {
   title: 'Dealer/Template/NotFoundTemplate',
   component: NotFoundTemplate,
-  decorators: [
-    (Story) => (
-      <BrowserRouter>
-        <Story />
-      </BrowserRouter>
-    ),
-    withTests({ results }),
-  ],
+  decorators: [withTests({ results })],
   parameters: { jest: ['NotFoundTemplate.int.test.js'] },
   excludeStories: ['custom'],
 }
 
 export function regular() {
-  return <NotFoundTemplate {...props} />
+  return (
+    <MemoryRouter>
+      <NotFoundTemplate />
+    </MemoryRouter>
+  )
 }
