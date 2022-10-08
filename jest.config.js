775f516e483718ae51fa9d5efff2e4a89c5411ff
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-10-08 13:13:58 +0200

    replace jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,44 +1,33 @@
-module.exports = {
-  testMatch: ['**/?(*.)test.js?(x)'],
-  testPathIgnorePatterns: process.env.NODE_ENV === 'test' ? ['packages/core/webpack/webpack.test.babel.js'] : ['packages/core'],
-  roots: ['<rootDir>'],
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-    '^.+\\.mdx$': '@storybook/addon-docs/jest-transform-mdx',
-  },
-  coverageDirectory: './coverage/',
-  collectCoverage: process.env.NODE_ENV === 'test',
-  coverageReporters: ['lcov', 'text'],
-  collectCoverageFrom: [
-    '!**/coverage/**',
-    '!**/node_modules/**',
-    'packages/**/*.{js,jsx}',
-    '!packages/**/index.js',
-    '!packages/**/*.prop.js',
-    '!packages/**/*.story.js',
-    '!packages/**/*.test.js',
-    '!packages/**/build/**',
-    '!packages/**/node_modules/**',
-    '!packages/**/webpack*',
-    '!packages/core/src/library/**',
-    '!packages/core/src/state/**',
-    '!packages/core/src/store/reducer/**',
-    '!packages/core/src/store/thunk/**',
-    '!packages/docs/**',
-    '!packages/e2e/**',
-  ],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/.mock/file.js',
-    '\\.(css|less|scss)$': 'identity-obj-proxy',
-  },
-  moduleFileExtensions: ['js', 'jsx'],
-  coverageThreshold: {
-    global: {
-      statements: 87,
-      branches: 55,
-      functions: 86,
-      lines: 85,
-    },
+/* eslint-disable prefer-destructuring */
+const getJestSetup = require('@papillonbits/library/jest').getJestSetup
+
+const testPathIgnorePatterns = process.env.NODE_ENV === 'test' ? ['packages/core/webpack/webpack.test.babel.js'] : ['packages/core']
+const collectCoverage = process.env.NODE_ENV === 'test'
+const collectCoverageFrom = [
+  '!**/coverage/**',
+  '!**/node_modules/**',
+  'packages/**/*.{js,jsx}',
+  '!packages/**/index.js',
+  '!packages/**/*.prop.js',
+  '!packages/**/*.story.js',
+  '!packages/**/*.test.js',
+  '!packages/**/build/**',
+  '!packages/**/node_modules/**',
+  '!packages/**/webpack*',
+  '!packages/core/src/library/**',
+  '!packages/core/src/state/**',
+  '!packages/core/src/store/reducer/**',
+  '!packages/core/src/store/thunk/**',
+  '!packages/docs/**',
+  '!packages/e2e/**',
+]
+const coverageThreshold = {
+  global: {
+    statements: 87,
+    branches: 55,
+    functions: 86,
+    lines: 85,
   },
 }
+
+module.exports = getJestSetup({ testPathIgnorePatterns, collectCoverage, collectCoverageFrom, coverageThreshold })
